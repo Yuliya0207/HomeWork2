f3f998191a6cf7530842641d399133338acce93d
Юлія <buba150310@gmail.com>
2021-07-18 23:55:47 +0300

    ex 2

--- ex..py
+++ ex..py
@@ -14,4 +14,13 @@ if __name__ =='__main__':
     print(minus(4,2))
 
 
+def newFunctionTest2(a, b):
+    return a - b
+
+
+if __name__ == '__main__':
+    print(plus(2, 2))
+    print(minus(4, 2))
+
+
 
