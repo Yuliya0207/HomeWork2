8f09a059833836611bf1d26a3e21e63bfcab8341
Юлія <buba150310@gmail.com>
2021-07-28 22:54:13 +0300

    homework

--- exampl 13.py
+++ exampl 13.py
@@ -1,16 +1,12 @@
 from random import randrange
 
+def a (y, x):
+    return [randrange(0, x) for _ in range(y)]
 
-def createList (iterationCount, maxRandomValue):
-    return [randrange(0, maxRandomValue) for x in range(iterationCount)]
-
-
-
-
-def createListsAndPrintDifferences():
-    lis3 = createList(15, 15)
-    lis4 = createList(15, 15)
+def compareLists ():
+    lis3 = a(15, 15)
+    lis4 = a(15, 15)
     listWithDifferentItems = set(lis3).intersection(lis4)
     print(listWithDifferentItems)
 
-createListsAndPrintDifferences()
+compareLists ()
