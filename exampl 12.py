09754d5a6c9b9e921138dde8b04c9af4e95c6fcc
Юлія <buba150310@gmail.com>
2021-07-29 21:28:56 +0300

    homework

---
+++ exampl 12.py
@@ -0,0 +1,14 @@
+a = ['kiev','obukhov', 'kagarlyk', 'mironovka']
+#'kiev' - 44,
+#'obukhov' - 4572,
+#'kagarlyk' - 4573,
+#'mironovka' - 4574
+
+b = {
+    #key:value,
+    'kiev': 44,
+    'obukhov':4572,
+    'kagarlyk':4573,
+    'mironovka':4574
+}
+print(b)
